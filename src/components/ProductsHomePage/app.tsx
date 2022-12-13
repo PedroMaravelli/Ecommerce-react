f512f87adeffae88dab622a313e6df6045b99f09
PedroMaravelli <phmaravelli123@gmail.com>
2022-12-13 17:23:26 -0300

    apply api fetch

--- src/components/ProductsHomePage/app.tsx
+++ src/components/ProductsHomePage/app.tsx
@@ -1,8 +1,18 @@
 import './index.css'
 import tShirts from '../../assets/t-shirts.png'
 import ButtonBuy from '../Buttons/ButtonBuy/ButtonBuy'
+import { useState } from 'react'
+import api from '../../services/api'
 
-function ProductsHomePage (){
+interface homeProps{
+    nome:string,
+    preco:number,
+    imagem:string
+}
+
+
+function ProductsHomePage ({nome,preco,imagem}: homeProps){
+
     return(
         <div className='containerDadProducts'>
             <section className='containerCard' >
@@ -10,28 +20,17 @@ function ProductsHomePage (){
                     <div className='barDisiving'></div>
                     <h2>MAIS VENDIDOS</h2>
                 </article>
+                
                 <article className='cardProducts'>
-                    <div className="card">
-                        <img src={tShirts} alt='imagem Camiseta'></img>
-                        <p>Camiseta Lisa Branca</p>
-                        <p>R$ 79.90</p>
-                        <a href="/"><ButtonBuy title='COMPRAR'></ButtonBuy></a>
-                    </div>
-                    <div className="card">
-                        <img src={tShirts} alt='imagem Camiseta'></img>
-                        <p>Camiseta Lisa Branca</p>
-                        <p>R$ 79.90</p>
-                        <a href="/"><ButtonBuy title='COMPRAR'></ButtonBuy></a>
+                    
 
-                    </div>
-                    <div className="card">
-                        <img src={tShirts} alt='imagem Camiseta'></img>
-                        <p>Camiseta Lisa Branca</p>
-                        <p>R$ 79.90</p>
-                        <a href="/"></a>
-                        <a href="/"><ButtonBuy title='COMPRAR'></ButtonBuy></a>
+                            <div className="card">
+                            <img src={tShirts} alt='imagem Camiseta'></img>
+                            <p>{nome}</p>
+                            <p>{preco}</p>
+                            <a href="/"><ButtonBuy title='COMPRAR'></ButtonBuy></a>
+                        </div>
 
-                    </div>
                         
 
                 </article>
--- src/screens/homePage.tsx
+++ src/screens/homePage.tsx
@@ -1,16 +1,36 @@
 import Header from "../components/Header/app";
 import React from "react";
+import api from "../services/api";
 import Carrosel from "../components/Carrosel/app";
 import ProductsHomePage from "../components/ProductsHomePage/app";
 import ContactsNews from "../components/ContactsNews/app";
 import Footer from "../components/Footer/app";
+import { useState } from "react";
+
 
 function HomePage (){
+
+
+let [products, setProducts] = useState<any[]>([])
+
+fetch('http://localhost:3000/')
+.then(response => response.json)
+.then(data => console.log(data))
+
+
+
+
+
     return(
     <>
         <Header></Header>
         <Carrosel></Carrosel>
-        <ProductsHomePage></ProductsHomePage>
+        {products.map((p)=>{
+            <ProductsHomePage nome={p.nome} preco={p.preco} imagem={p.imagem}/>
+
+        })}
+        
+      
         <ContactsNews></ContactsNews>
         <Footer></Footer>
 
