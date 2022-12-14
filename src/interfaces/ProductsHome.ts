a7e62acc1be315207f49efc53ee342530a00921b
PedroMaravelli <phmaravelli123@gmail.com>
2022-12-14 15:15:46 -0300

    create hook get products

---
+++ src/interfaces/ProductsHome.ts
@@ -0,0 +1,12 @@
+export interface ProductsHome {
+    id: number;
+    SKU: string;
+    nome: string;
+    descricao: string;
+    preco: string;
+    cor: string;
+    tamanho: string;
+    genero: string;
+    quantidade: number;
+    imagem: string;
+}
